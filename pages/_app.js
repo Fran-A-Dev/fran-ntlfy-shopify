5ebb7e6fcb6c1d0c8c1e371031e37bb9ee7ecb3e
Francis Agulto <francisagulto@franciss-MBP.attlocal.net>
2022-01-02 14:47:02 -0600

    no swiper

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,6 @@ import "tailwindcss/tailwind.css";
 import Layout from "../components/Layout";
 import ShopProvider from "../context/shopContext";
 import { useRouter } from "next/router";
-import "swiper/components/navigation/navigation.scss";
-import "swiper/components/pagination/pagination.scss";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
